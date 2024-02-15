8b7e189b9eac483f119a52b9d85615f143fa27b8
pachecoio <thiagodelimapacheco@gmail.com>
2024-02-15 08:39:51 -0500

    included hyper config install ubuntu

--- dotfiles/hyper/.hyper.js
+++ dotfiles/hyper/.hyper.js
@@ -8,7 +8,7 @@ module.exports = {
         // or `'canary'` for less polished but more frequent updates
         updateChannel: 'stable',
         // default font size in pixels for all tabs
-        fontSize: 15,
+        fontSize: 13,
         // font family with optional fallbacks
         fontFamily: '"NotoMono Nerd Font Mono", "Menlo", "DejaVu Sans Mono", "Lucida Console", monospace',
         // default font weight: 'normal' or 'bold'
